ee7841bdf4358f8e5c9521477c2dce148f29a300
dakaii <daiki815@gmail.com>
2016-11-27 19:36:55 +0900

    work in progress

--- src/database/database.go
+++ src/database/database.go
@@ -1,18 +1,56 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"time"
+
 	"github.com/boltdb/bolt"
 )
 
+type BoltDB struct {
+	db *bolt.DB
+}
+
 //OpenDB set up the database.
-func OpenDB() {
+func OpenDB() (boltDB *BoltDB, err error) {
 	// Open the my.db data file in your current directory.
 	// It will be created if it doesn't exist.
-	db, err := bolt.Open("my.db", 0600, nil)
+	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	boltDB = &BoltDB{db}
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	//defer boltDB.db.Close()
+	return boltDB, err
+}
 
+func InitDB(boltDB *BoltDB) {
+	boltDB.db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucket([]byte("MyBucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+}
+
+func WriteSomethingToDB(boltDB *BoltDB, key string) error {
+	var err error
+	boltDB.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		err = b.Put([]byte("answer"), []byte("42"))
+		return nil
+	})
+	return err
+}
+
+func RetrieveSomethingFromDB(boltDB *BoltDB, key string) string {
+	var value []byte
+	boltDB.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		value = b.Get([]byte("answer"))
+		return nil
+	})
+	return string(value)
 }
--- src/main.go
+++ src/main.go
@@ -12,13 +12,46 @@ import (
 func main() {
 	// Open the my.db data file in your current directory.
 	// It will be created if it doesn't exist.
-	database.OpenDB()
+	//boltDB = database.BoltDB
+	boltDB, _ := database.OpenDB()
+	database.InitDB(boltDB)
+	//database.WriteSomethingToDB(boltDB)
+	//database.RetrieveSomethingFromDB(boltDB)
 	router := gin.Default()
 
-	router.GET("/:arg", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Connection Established")
+	})
+
+	router.GET("/test/:arg", func(c *gin.Context) {
 		param := c.Param("arg")
 		c.String(http.StatusOK, "Hello %s", param)
 	})
+
+	router.POST("/save/:arg", func(c *gin.Context) {
+		key := c.Param("arg")
+		value := database.WriteSomethingToDB(boltDB, key)
+		if value != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"key":   key,
+				"error": value,
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"key": key,
+			})
+		}
+	})
+
+	router.GET("/retrieve/:arg", func(c *gin.Context) {
+		key := c.Param("arg")
+		value := database.RetrieveSomethingFromDB(boltDB, key)
+		c.JSON(http.StatusOK, gin.H{
+			"key":   key,
+			"value": value,
+		})
+	})
+
 	// Simple group: v1
 	//cache := router.Group("/cache")
 	//{
